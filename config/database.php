88fdf31cc3e6194c148ed59b278858b7feb9282d
imran Niaz <acy221005@myu.edu.pk>
2024-06-14 02:51:40 +0500

    New uPDATE

--- config/database.php
+++ config/database.php
@@ -91,6 +91,21 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mysql2' => [
+           'driver' => 'mysql',
+           'host' => env('DB2_HOST', '127.0.0.1'),
+           'port' => env('DB2_PORT', '3306'),
+           'database' => env('DB2_DATABASE', 'forge'),
+           'username' => env('DB2_USERNAME', 'forge'),
+           'password' => env('DB2_PASSWORD', ''),
+           'unix_socket' => env('DB2_SOCKET', ''),
+           'charset' => 'utf8mb4',
+           'collation' => 'utf8mb4_unicode_ci',
+           'prefix' => '',
+           'strict' => true,
+           'engine' => null,
+       ],
+
     ],
 
     /*
